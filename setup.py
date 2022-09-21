0c540caf54ebd9e0b5184652d62919cd62e2701c
Itay Levy <levyitay@gmail.com>
2022-09-21 09:09:10 -0700

    Update setup py

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt", "r") as fh:
     requirements = [line.strip() for line in fh]
 
 setup(
-    name='ironsource_api',
+    name='mobile-api-lib-python',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     package_dir={'':'ironsource_api'},
     packages=find_packages("ironsource_api",exclude=['tests']),
     url='https://github.com/ironSource/mobile-api-lib-python',
-    license='MIT',
-    author='IronSource Mobile',
+    license='Apache-2.0',
+    author='IronSource Ltd.',
     author_email='mobile-api-lib-ci@is.com',
     long_description=long_description,
     long_description_content_type="text/markdown",
